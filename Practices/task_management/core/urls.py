22023d68d42fe6cbc0fc802d1016e8e035b11a26
aisultankhalelov <aisultan.khalelov@gmail.com>
2025-02-14 00:51:13 +0500

    added cv task

--- Practices/task_management/core/urls.py
+++ Practices/task_management/core/urls.py
@@ -2,7 +2,10 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 
-from .views import UserViewSet, ProjectViewSet, CategoryViewSet, PriorityViewSet, TaskViewSet
+from task_management import settings
+from .views import UserViewSet, ProjectViewSet, CategoryViewSet, PriorityViewSet, TaskViewSet, contact_view, \
+    success_view, create_cv, cv_list, share_cv_email
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -14,6 +17,11 @@ router.register(r'tasks', TaskViewSet)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+    path('contact/', contact_view, name='contact'),
+    path('success/', success_view, name='success_page'),
+    path('create_cv/', create_cv, name='create_cv'),
+    path('cv/', cv_list, name='cv_list'),
+    path('share/email/<int:cv_id>/', share_cv_email, name='share_cv_email'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += router.urls
--- Practices/task_management/task_management/settings.py
+++ Practices/task_management/task_management/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -157,3 +158,15 @@ LOGGING = {
 }
 
 AUTH_USER_MODEL = 'core.User'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'aisultan.khalelov@gmail.com'
+EMAIL_HOST_PASSWORD = 'bohgaulywcoghdpt'
